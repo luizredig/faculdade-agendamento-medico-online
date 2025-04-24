37ba28bf93d0581ed45755e77b0a27dda7eb6bee
redig <luizredig@gmail.com>
2025-04-24 10:09:31 -0300

    feat: adiciona mock de hospitais

---
+++ api-agendamentos/hospitais.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { id: 1, nome: "Hospital Central" },
+  { id: 2, nome: "Clínica Vida Saudável" },
+  { id: 3, nome: "Hospital São Lucas" },
+  { id: 4, nome: "Instituto Médico Popular" },
+  { id: 5, nome: "Hospital Nova Esperança" },
+];
